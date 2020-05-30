595fd8fc51546da6d291b74a743037caa0ef0822
Robert <robert.bonagura@gmail.com>
2020-05-30 16:03:21 -0400

    Add aws s3 configuration

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,12 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-s3`,
+      options: {
+        bucketName: "bright-eyed-solutions"
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `src`,
