ce387dc20ad5a4ca0fd888212acee8592f33c0a6
Robert <robert.bonagura@gmail.com>
2020-06-02 03:26:54 -0400

    Modularize welcome component

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -1,13 +1,13 @@
 import React from "react"
 import welcomeStyles from "./styles/welcome.module.css"
 
-export default () => (
+export default ( props ) => (
   <div className={ welcomeStyles.container }>
     <div className={ welcomeStyles.photoContainer }>
       <div className={ welcomeStyles.text }>
         <h3>Hello, welcome to</h3>
-        <h1>Bright Eyed Solutions</h1>
-        <p>Where quality matters</p>
+        <h1>{props.title}</h1>
+        <p>{props.subtitle}</p>
       </div>
     </div>
   </div>
